4c8016d0c414b5837d8a48c3c32b75f0b63e06c2
L3b1n <binleo07@icloud.com>
2022-10-24 13:09:47 +0300

    Add some stylistic fixes

--- Operating systems (C++ & WinAPI)/Lab_3/Bin_task_3.cpp
+++ Operating systems (C++ & WinAPI)/Lab_3/Bin_task_3.cpp
@@ -27,13 +27,13 @@ DWORD WINAPI work(void* obj)
     cout << "Enter time interval to sleep: ";
     cin >> time;
 
-    for (int i = 0; i < dArr->size; i++)
+    for(int i = 0; i < dArr->size; i++)
     {
-        if (!isdigit(array[i]) && !isalpha(array[i])) { result.push_front(array[i]); }
-        else { result.push_back(' '); }
+        if(!isdigit(array[i]) && !isalpha(array[i])){ result.push_front(array[i]);}
+        else{ result.push_back(' ');}
     }
 
-    for (int i = 0; i < dArr->size; i++) { cout << result[i] << " "; Sleep(time); }
+    for(int i = 0; i < dArr->size; i++){ cout << result[i] << " "; Sleep(time);}
     cout << "\nThread work is finished" << endl;
     SetEvent(hOutEvent);
     return 0;
@@ -48,9 +48,9 @@ DWORD WINAPI CountElement(void* obj)
     cout << "Thread CountElement is started" << endl;
 
     counter = 0;
-    for (int i = 0; i < dArr->size; i++)
+    for(int i = 0; i < dArr->size; i++)
     {
-        if (result[i] == '?'  result[i] == '.'  result[i] == ','  result[i] == ':'  result[i] == '!' || result[i] == ';') { counter++; }
+        if(result[i] == '?' || result[i] == '.' || result[i] == ',' || result[i] == ':' || result[i] == '!' || result[i] == ';'){ counter++;}
     }
 
     LeaveCriticalSection(&cs);
@@ -65,10 +65,10 @@ int main()
     cin >> size;
     char* array = new char[size];
     cout << "Enter " << size << " elements of array: ";
-    for (int i = 0; i < size; i++) { cin >> array[i]; }
+    for(int i = 0; i < size; i++){ cin >> array[i];}
     cout << "Size of array is: " << size << endl;
     cout << "Elements of array is: ";
-    for (int i = 0; i < size; i++) { cout << array[i] << " "; } cout << endl;
+    for(int i = 0; i < size; i++){ cout << array[i] << " ";} cout << endl;
     cout << "Enter number K: ";
     cin >> k;
     KData* temp = new KData();
@@ -83,17 +83,17 @@ int main()
     DWORD IDThreadWork;
     DWORD dwCount;
     hThreadWork = CreateThread(NULL, 0, work, (void*)temp, NULL, &IDThreadWork);
-    if (hThreadWork == nullptr) { return GetLastError(); }
+    if(hThreadWork == nullptr){ return GetLastError();}
 
     HANDLE hThreadCount;
     DWORD IDThreadCount;
     DWORD dwCountCount;
     hThreadCount = CreateThread(NULL, 0, CountElement, (void*)temp, NULL, &IDThreadCount);
-    if (hThreadCount == nullptr) { return GetLastError(); }
+    if(hThreadCount == nullptr){ return GetLastError();}
 
     WaitForSingleObject(hOutEvent, INFINITE);
 
-    for (int i = 0; i < size; i++) { cout << result[i] << " "; }
+    for(int i = 0; i < size; i++){ cout << result[i] << " ";}
     cout << endl;
 
     SetEvent(hContinueEvent);
