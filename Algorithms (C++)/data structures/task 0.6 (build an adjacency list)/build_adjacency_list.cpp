ff402f77cc829ef2f9876caf3736690023d531e6
L3b1n <binleo07@icloud.com>
2022-11-03 12:32:06 +0300

    Add task build adjacency list

---
+++ Algorithms (C++)/data structures/task 0.6 (build an adjacency list)/build_adjacency_list.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt", ios::out);
+    ofstream out("output.txt");
+    ios_base::sync_with_stdio(0);
+    in.tie(0);
+    out.tie(0);
+    int n, m, i = 0;
+    in >> n >> m;
+    std::vector<std::vector<int>> answer(n, std::vector<int>());
+    for_each(answer.begin(), (answer.begin() + m), 
+        [&in, &answer, &i](const std::vector<int>& temp){
+            int num1, num2;
+            in >> num1 >> num2;
+            answer[num1 - 1].push_back(num2);
+            answer[num2 - 1].push_back(num1);
+        }
+    );
+    for_each(answer.begin(), answer.end(), [&out, &i](const std::vector<int>& vec){ out << vec.size() << " "; std::copy(vec.rbegin(), vec.rend(), std::ostream_iterator<int>(out, " ")); out << endl;});
+}
