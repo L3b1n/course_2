4c1cb746d3f1b11a80e5d5bd7c67e92f6d28c5d1
L3b1n <binleo07@icloud.com>
2022-11-03 20:51:48 +0300

    Some fixes

--- Algorithms (C++)/data structures/task 0.6 (build an adjacency list)/build_adjacency_list.cpp
+++ Algorithms (C++)/data structures/task 0.6 (build an adjacency list)/build_adjacency_list.cpp
@@ -4,10 +4,11 @@
 
 using namespace std;
 
+ifstream in("input.txt", ios::out);
+ofstream out("output.txt");
+
 int main()
 {
-    ifstream in("input.txt", ios::out);
-    ofstream out("output.txt");
     ios_base::sync_with_stdio(0);
     in.tie(0);
     out.tie(0);
@@ -15,12 +16,12 @@ int main()
     in >> n >> m;
     std::vector<std::vector<int>> answer(n, std::vector<int>());
     for_each(answer.begin(), (answer.begin() + m), 
-        [&in, &answer, &i](const std::vector<int>& temp){
+        [&answer, &i](const std::vector<int>& temp){
             int num1, num2;
-            in >> num1 >> num2;
+            ::in >> num1 >> num2;
             answer[num1 - 1].push_back(num2);
             answer[num2 - 1].push_back(num1);
         }
     );
-    for_each(answer.begin(), answer.end(), [&out, &i](const std::vector<int>& vec){ out << vec.size() << " "; std::copy(vec.rbegin(), vec.rend(), std::ostream_iterator<int>(out, " ")); out << endl;});
+    for_each(answer.begin(), answer.end(), [&i](const std::vector<int>& vec){ ::out << vec.size() << " "; std::copy(vec.rbegin(), vec.rend(), std::ostream_iterator<int>(out, " ")); ::out << endl;});
 }
