46bb34753cac6ae5f98804e60b14d87e89e237c8
L3b1n <binleo07@icloud.com>
2022-11-03 20:50:36 +0300

    SSome fixes

--- Algorithms (C++)/data structures/task 0.7 (canonical view)/canonical_view.cpp
+++ Algorithms (C++)/data structures/task 0.7 (canonical view)/canonical_view.cpp
@@ -4,10 +4,11 @@
 
 using namespace std;
 
+ifstream in("input.txt", ios::out);
+ofstream out("output.txt");
+
 int main()
 {
-    ifstream in("input.txt", ios::out);
-    ofstream out("output.txt");
     ios_base::sync_with_stdio(0);
     in.tie(0);
     out.tie(0);
@@ -15,11 +16,11 @@ int main()
     in >> n;
     std::vector<int> answer(n, 0);
     for_each(answer.begin(), answer.end(), 
-        [&in, &out, &answer, &j](int& temp){
+        [&answer, &j](int& temp){
             int i = 0; j++;
             for_each(answer.begin(), answer.end(), 
-                [&in, &answer, &i, &j](int& x){ 
-                    int temp; in >> temp; if(temp == 1){ answer[i] = j;} i++;
+                [&answer, &i, &j](int& x){ 
+                    int temp; ::in >> temp; if(temp == 1){ answer[i] = j;} i++;
                 }
             );
         }
