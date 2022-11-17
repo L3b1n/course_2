f729466212f380bfced75ccd78e87e990c7344fe
L3b1n <binleo07@icloud.com>
2022-11-17 10:45:25 +0300

    Add task of BFS

--- Algorithms (C++)/data structures/task 0.8 (BFS)/BFS.cpp
+++ Algorithms (C++)/data structures/task 0.8 (BFS)/BFS.cpp
@@ -1,28 +1,44 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 #include <fstream>
 #include <algorithm>
 
-using namespace std;
-
-ifstream in("input.txt", ios::out);
-ofstream out("output.txt");
+std::ifstream in("input.txt", std::ios::out);
+std::ofstream out("output.txt");
 
 int main()
 {
     std::ios_base::sync_with_stdio(0);
     in.tie(0);
     out.tie(0);
-    int n, m;
-    in >> n >> m;
-    std::vector<std::vector<bool>> answer(n, std::vector<bool>(n, 0));
-    std::for_each(answer.begin(), (answer.begin() + m), 
-        [&answer](const std::vector<bool>& temp){
-            int num1, num2;
-            ::in >> num1 >> num2;
-            answer[num1 - 1][num2 - 1] = 1;
-            answer[num2 - 1][num1 - 1] = 1;
+    int n, k = 1;
+    in >> n;
+    std::vector<int> priority(n, 0); 
+    std::vector<std::vector<int>> matrix(n, std::vector<int>(n, 0));
+    std::for_each(matrix.begin(), matrix.end(), 
+        [](std::vector<int>& vec){
+            std::for_each(vec.begin(), vec.end(), [](int& temp){ ::in >> temp;});
         }
     );
-    std::for_each(answer.begin(), answer.end(), [](const std::vector<bool>& vec){ std::copy(vec.begin(), vec.end(), std::ostream_iterator<bool>(out, "  ")); out << endl;});
+    for(int j = 0; j < n; j++)
+    {
+        if(!priority[j])
+        {
+            std::queue<int> temp;
+            temp.push(j);
+            priority[j] = k++;
+            while(!temp.empty())
+            {
+                int i = 0;
+                std::for_each(matrix[temp.front()].begin(), matrix[temp.front()].end(), 
+                    [&i, &temp, &k, &priority](int& num){
+                        if(num){ if(!priority[i]){ priority[i] = k++; temp.push(i);}} i++;
+                    }
+                );
+                temp.pop();
+            }
+        }
+    }
+    std::copy(priority.begin(), priority.end(), std::ostream_iterator<int>(out, " "));
 }
