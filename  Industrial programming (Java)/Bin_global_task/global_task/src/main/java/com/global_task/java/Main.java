da9d4df95725fc53b318b334ed38c6b569d491cb
L3b1n <binleo07@icloud.com>
2022-12-25 18:40:18 +0300

    Some fixes in main

---
+++  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/java/Main.java
@@ -0,0 +1,42 @@
+package com.global_task.java;
+
+import com.global_task.java.FileReaders.FileBuilder;
+import com.global_task.contracts.Interface.InterfaceFileReader;
+import com.global_task.java.FileReaders.JSONFileReader;
+import com.global_task.java.FileReaders.TXTFileReader;
+import com.global_task.java.FileReaders.XMLFileReader;
+
+public class Main {
+    private static final String FirstKey = "squirreldgwndlaj";
+    private static final String SecondKey = "abshgsfrenhlksmp";
+
+    private static final String ZippedAndEncrypedInputFileName = "input.zip";
+    private static final String ZippedAndEncrypedOutputFileName = "encrypted_output.txt";
+
+    private static final String TxtInputFileName = "input.txt";
+    private static final String TxtOutputFileName = "output.txt";
+
+    private static final String JsonInputFileName = "input.json";
+    private static final String JsonOutputFileName = "output.json";
+
+    private static final String XmlInputFileName = "input.xml";
+    private static final String XmlOutputFileName = "output.xml";
+    
+    public static void main(String[] args) throws Throwable {
+        TXTFileReader tempTxt = new TXTFileReader(TxtInputFileName);
+        tempTxt.getResult(TxtOutputFileName);
+
+        FileBuilder builder = new FileBuilder("Txt", ZippedAndEncrypedInputFileName);
+        builder.setEncrypt(FirstKey);
+        builder.setEncrypt(SecondKey);
+        builder.setZip(true);
+        InterfaceFileReader readerTxt = builder.getFileReader();
+        readerTxt.getResult(ZippedAndEncrypedOutputFileName); 
+        
+        JSONFileReader tempJson = new JSONFileReader(JsonInputFileName);
+        tempJson.getResult(JsonOutputFileName);
+        
+        XMLFileReader tempXml = new XMLFileReader(XmlInputFileName);
+        tempXml.getResult(XmlOutputFileName);
+    }
+}
