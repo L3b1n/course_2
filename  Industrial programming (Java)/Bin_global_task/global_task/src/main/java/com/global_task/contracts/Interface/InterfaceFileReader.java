1d328a7fe52683b6666dbfff83c284b017a966b3
L3b1n <binleo07@icloud.com>
2022-12-25 18:12:59 +0300

    Add some fixes in InterfaceFileReader

---  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/contracts/Interface/InterfaceFileReader.java
+++  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/contracts/Interface/InterfaceFileReader.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 
 public interface InterfaceFileReader {
     String getFileName();
-    ArrayList<ArrayList<String>> Read() throws Throwable;
     void Write(ArrayList<ArrayList<String>> result, String outputFileName) throws Throwable;
+    ArrayList<ArrayList<String>> Read() throws Throwable;
+    ArrayList<ArrayList<String>> Transform(ArrayList<ArrayList<String>> readFile) throws Throwable;
     ArrayList<ArrayList<String>> Calculate(ArrayList<ArrayList<String>> readFile) throws Throwable;
     void getResult(String outputFileName) throws Throwable;
 }
