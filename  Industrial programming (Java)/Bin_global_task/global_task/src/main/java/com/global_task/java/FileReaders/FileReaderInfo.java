6c1dfc80d4215fc095f7ac393adbb5f335a2087c
L3b1n <binleo07@icloud.com>
2022-12-25 16:02:17 +0300

    Add fixed file FileReaderInfo

---
+++  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/java/FileReaders/FileReaderInfo.java
@@ -0,0 +1,33 @@
+package com.global_task.java.FileReaders;
+
+import java.util.ArrayList;
+
+import com.global_task.contracts.Interface.InterfaceFileReader;
+
+public class FileReaderInfo implements InterfaceFileReader {
+    protected String inputName;
+    public FileReaderInfo(String inputFileName) {
+        this.inputName = inputFileName;
+    }
+
+    @Override
+    public String getFileName() {
+        return this.inputName;
+    }
+
+    @Override
+    public ArrayList<ArrayList<String>> Read() throws Throwable { 
+        return null;
+    }
+
+    @Override
+    public void Write(ArrayList<ArrayList<String>> result, String outputFileName) throws Throwable {}
+
+    @Override
+    public ArrayList<ArrayList<String>> Calculate(ArrayList<ArrayList<String>> readFile) throws Throwable { 
+        return null;
+    }
+
+    @Override
+    public void getResult(String outputFileName) throws Throwable {}
+}
