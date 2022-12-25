d5b1bd534e90cd761b7df806086b1c86b140327f
L3b1n <binleo07@icloud.com>
2022-12-25 18:36:17 +0300

    Add new file FileReader

---
+++  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/java/FileReaders/FileReader.java
@@ -0,0 +1,17 @@
+package com.global_task.java.FileReaders;
+
+import com.global_task.contracts.Interface.InterfaceFileReader;
+
+public abstract class FileReader implements InterfaceFileReader{
+    protected String inputName;
+    protected InterfaceFileReader reader;
+    public FileReader(InterfaceFileReader reader) {
+        this.reader = reader;
+        inputName = reader.getFileName();
+    }
+
+    @Override
+    public String getFileName() {
+        return this.inputName;
+    }
+}
