d6e0c33c5ca46718934fa9427959e8f6d4388f73
L3b1n <binleo07@icloud.com>
2022-12-31 18:17:50 +0300

    Add some fixes in FileReaderBuilder

---  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/contracts/Interface/FileReaderBuilder.java
+++  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/contracts/Interface/FileReaderBuilder.java
@@ -1,7 +1,10 @@
 package com.global_task.contracts.Interface;
 
+import java.util.ArrayList;
+
 public interface FileReaderBuilder {
     void setEncrypt(String key);
+    void setEncrypt(ArrayList<String> keyList);
     void setZip(boolean isZip);
     InterfaceFileReader getFileReader();
 }
