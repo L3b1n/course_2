b5f4d26582ce463b53954f9cbcf52560486b6e64
L3b1n <binleo07@icloud.com>
2022-12-31 21:18:39 +0300

    Create new folder and move ZippedFileReader

---  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/FileReaders/ZippedFileReader.java
+++  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/FileReaders/ZippedFileReader.java
@@ -1,15 +1,31 @@
-package com.global_task.java.FileReaders;
+package com.global_task.FileReaders;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 import com.global_task.contracts.Interface.InterfaceFileReader;
 
 public class ZippedFileReader extends FileReader {
     public ZippedFileReader(InterfaceFileReader fileReader) {
         super(fileReader);
+    }
+
+    public static void WriteZipped(String inputFileName, String outputFileName) throws Throwable {
+        ZipOutputStream writer = new ZipOutputStream(new FileOutputStream(outputFileName));
+        FileInputStream reader = new FileInputStream(inputFileName);
+        ZipEntry entry1 = new ZipEntry(inputFileName);
+        writer.putNextEntry(entry1);
+        byte[] buffer = new byte[reader.available()];
+        reader.read(buffer);
+        writer.write(buffer);
+        writer.closeEntry();
+        writer.close();
+        reader.close();
     }
 
     @Override
@@ -20,30 +36,30 @@ public class ZippedFileReader extends FileReader {
     @Override
     public ArrayList<ArrayList<String>> Read() throws Throwable {
         byte[] buffer = new byte[1024];
-        ArrayList<ArrayList<String>> readFile = new ArrayList<>();
         ZipInputStream zipReader = new ZipInputStream(new FileInputStream(inputName));
+        StringBuilder builder = new StringBuilder();
         while(zipReader.getNextEntry() != null) {
             int length;
             while((length = zipReader.read(buffer)) > 0) {
-                ArrayList<String> temp = new ArrayList<>();
-                temp.add(new String(buffer, 0, length, StandardCharsets.UTF_8));
-                readFile.add(temp);
+                String tempString = new String(buffer, 0, length, StandardCharsets.UTF_8);
+                builder.append(tempString);
             }
         }
         zipReader.close();
+        byte[] tempByte = builder.toString().getBytes(StandardCharsets.UTF_8);
+        return reader.Transform(tempByte);
+    }
+    
+    @Override
+    public ArrayList<ArrayList<String>> Transform(byte[] readFile) throws Throwable {
         return reader.Transform(readFile);
     }
     
     @Override
-    public ArrayList<ArrayList<String>> Transform(ArrayList<ArrayList<String>> readFile) throws Throwable {
-        return reader.Transform(readFile);
-    }
-
-    @Override
     public ArrayList<ArrayList<String>> Calculate(ArrayList<ArrayList<String>> readFile) throws Throwable {
         return reader.Calculate(readFile);
     }
-
+    
     @Override
     public void getResult(String outputFileName) throws Throwable {
         ArrayList<ArrayList<String>> readFile = Read();
