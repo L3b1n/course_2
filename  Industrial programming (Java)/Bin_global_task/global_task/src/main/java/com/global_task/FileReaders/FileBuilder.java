787eb08201a3556d3b6b038f9b7e0f9c5e185235
L3b1n <binleo07@icloud.com>
2022-12-31 21:18:03 +0300

    Create new folder and move FileBuilder

---  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/FileReaders/FileBuilder.java
+++  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/FileReaders/FileBuilder.java
@@ -1,4 +1,6 @@
-package com.global_task.java.FileReaders;
+package com.global_task.FileReaders;
+
+import java.util.ArrayList;
 
 import com.global_task.contracts.Interface.FileReaderBuilder;
 import com.global_task.contracts.Interface.InterfaceFileReader;
@@ -6,18 +8,30 @@ import com.global_task.contracts.Interface.InterfaceFileReader;
 public class FileBuilder implements FileReaderBuilder{
     protected InterfaceFileReader fileReader;
     public FileBuilder(String fileType, String fileName) {
-        if(fileType == "Txt") {
+        if(fileType.equals("txt")) {
             fileReader = new TXTFileReader(fileName);
-        } else if(fileType == "Json") {
+        } else if(fileType.equals("json")) {
             fileReader = new JSONFileReader(fileName);
-        } else {
+        } else if(fileType.equals("xml")) {
             fileReader = new XMLFileReader(fileName);
+        }
+    }
+    
+    @Override
+    public void setEncrypt(String key) {
+        fileReader = new EncryptedFileReader(key, fileReader);
+    }
+    
+    @Override
+    public void setEncrypt(ArrayList<String> keyList) {
+        for(String key : keyList) {
+            fileReader = new EncryptedFileReader(key, fileReader);
         }
     }
 
     @Override
-    public void setEncrypt(String key) {
-        fileReader = new EncryptedFileReader(key, fileReader);
+    public void setCompressed(boolean isCompressed) {
+        if(isCompressed){ fileReader = new CompressFileReader(fileReader);}
     }
 
     @Override
