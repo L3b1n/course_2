fcc5884b0268c9006d775f652953ae38ac6a4317
L3b1n <binleo07@icloud.com>
2023-01-06 00:07:51 +0300

    Add some fixes in class Client

---  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/GUI/Client.java
+++  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/GUI/Client.java
@@ -1,29 +1,27 @@
 package com.global_task.GUI;
 
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.BufferedReader;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-// import java.io.RandomAccessFile;
 import java.net.ServerSocket;
 
 public class Client {
     private Socket client;
-    private InputStream inputStream;
     private OutputStream outputStream;
 
     public void connectServer(int serverPort) throws Throwable {
         try(ServerSocket server = new ServerSocket(serverPort)) {
             client = server.accept();
         } 
-        inputStream = client.getInputStream();
         outputStream = client.getOutputStream();
     }
 
@@ -31,37 +29,44 @@ public class Client {
         client.close();
     }
 
-    public void getFile() throws Throwable {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        // String fileName = reader.readLine();
-        // File tempFile = new File(fileName);
-        // RandomAccessFile writer = new RandomAccessFile(tempFile, "rw");
-        // int num = reader.read();
-        // while(num != -1) {
-        //     writer.write(num);
-        //     writer.skipBytes(num);
-        //     num = reader.read();
-        // }
-        // writer.close();
-        reader.close();
-    }
-
-    public void sendFile(File tempFile, String flag, String keys, String compressLevel) throws Throwable {
+    public void sendFile(File tempFile, File directoryFile, String flag, ArrayList<String> keys, String compressLevel) throws Throwable {
         PrintWriter writer = new PrintWriter(outputStream, true);
         writer.println(tempFile.getName());
+        writer.println(directoryFile.getAbsolutePath());
         writer.println(flag);
-        writer.println(keys);
+        writer.println(keys.size());
+        if(keys.size() != 0) {
+            for(String key : keys) {
+                writer.println(key);
+            }
+        }
         writer.println(compressLevel);
         System.out.println(tempFile.getName() + "  " + flag + "  " + keys);
-        Scanner in = new Scanner(new FileReader(tempFile));
-        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(outputStream));
-        while(in.hasNextLine()) {
-            String temp = in.nextLine();
-            out.write(temp + "\n");
-            out.flush();
+        if(tempFile.getName().substring(tempFile.getName().lastIndexOf('.') + 1).equals("zip")) {
+            ZipEntry entry;
+            ZipInputStream in = new ZipInputStream(new FileInputStream(tempFile));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(outputStream));
+            while((entry = in.getNextEntry()) != null) {
+                writer.println(entry.getName());
+                for(int c = in.read(); c != -1; c = in.read()) {
+                    out.write(c);
+                }
+                out.flush();
+                in.closeEntry();
+            }
+            out.close();
+            in.close();
+        } else {
+            Scanner in = new Scanner(new FileReader(tempFile));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(outputStream));
+            while(in.hasNextLine()) {
+                String temp = in.nextLine();
+                out.write(temp + "\n");
+                out.flush();
+            }
+            out.close();
+            in.close();
+            client.close();
         }
-        out.close();
-        in.close();
-        client.close();
     }
 }
