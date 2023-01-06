55c787e78220685ac8221c57b367ea42624f5463
L3b1n <binleo07@icloud.com>
2023-01-06 23:34:42 +0300

    Add new method in interface InterfaceFileReader

---  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/contracts/Interface/InterfaceFileReader.java
+++  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/contracts/Interface/InterfaceFileReader.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public interface InterfaceFileReader {
     String getFileName();
     void Write(ArrayList<ArrayList<String>> result, String outputFileName) throws Throwable;
+    void WriteResult(ArrayList<ArrayList<String>> result, String outputFileName) throws Throwable;
     ArrayList<ArrayList<String>> Read() throws Throwable;
     ArrayList<ArrayList<String>> Transform(byte[] tempByte) throws Throwable;
     ArrayList<ArrayList<String>> Calculate(ArrayList<ArrayList<String>> readFile) throws Throwable;
