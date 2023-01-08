65fda32b2a6a3e463e21df1c18996c83df834413
L3b1n <binleo07@icloud.com>
2023-01-08 15:13:03 +0300

    Add new throws for methods in class ZippedFileReader

---  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/FileReaders/ZippedFileReader.java
+++  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/FileReaders/ZippedFileReader.java
@@ -15,58 +15,66 @@ public class ZippedFileReader extends FileReader {
         super(fileReader);
     }
 
-    public static void WriteZipped(String inputFileName, String outputFileName) throws Throwable {
-        ZipOutputStream writer = new ZipOutputStream(new FileOutputStream(outputFileName));
-        FileInputStream reader = new FileInputStream(inputFileName);
-        ZipEntry entry = new ZipEntry(inputFileName);
-        writer.putNextEntry(entry);
-        byte[] buffer = new byte[reader.available()];
-        reader.read(buffer);
-        writer.write(buffer);
-        writer.closeEntry();
-        writer.close();
-        reader.close();
+    public static void WriteZipped(String inputFileName, String outputFileName) throws Exception {
+        try {
+            ZipOutputStream writer = new ZipOutputStream(new FileOutputStream(outputFileName));
+            FileInputStream reader = new FileInputStream(inputFileName);
+            ZipEntry entry = new ZipEntry(inputFileName);
+            writer.putNextEntry(entry);
+            byte[] buffer = new byte[reader.available()];
+            reader.read(buffer);
+            writer.write(buffer);
+            writer.closeEntry();
+            writer.close();
+            reader.close();
+        } catch(Exception e) {
+            throw new Exception("Error in file zip. Check selected file, actions and try again.", e);
+        }
     }
 
     @Override
-    public void Write(ArrayList<ArrayList<String>> result, String outputFileName) throws Throwable {
+    public void Write(ArrayList<ArrayList<String>> result, String outputFileName) throws Exception {
         reader.Write(result, outputFileName);
     }
 
     @Override
-    public void WriteResult(ArrayList<ArrayList<String>> result, String outputFileName) throws Throwable {
+    public void WriteResult(ArrayList<ArrayList<String>> result, String outputFileName) throws Exception {
         reader.WriteResult(result, outputFileName);
     }
 
     @Override
-    public ArrayList<ArrayList<String>> Read() throws Throwable {
-        byte[] buffer = new byte[1024];
-        ZipInputStream zipReader = new ZipInputStream(new FileInputStream(inputName));
-        StringBuilder builder = new StringBuilder();
-        while(zipReader.getNextEntry() != null) {
-            int length;
-            while((length = zipReader.read(buffer)) > 0) {
-                String tempString = new String(buffer, 0, length, StandardCharsets.UTF_8);
-                builder.append(tempString);
+    public ArrayList<ArrayList<String>> Read() throws Exception {
+        try {
+            byte[] buffer = new byte[1024];
+            ZipInputStream zipReader = new ZipInputStream(new FileInputStream(inputName));
+            StringBuilder builder = new StringBuilder();
+            while(zipReader.getNextEntry() != null) {
+                int length;
+                while((length = zipReader.read(buffer)) > 0) {
+                    String tempString = new String(buffer, 0, length, StandardCharsets.UTF_8);
+                    builder.append(tempString);
+                }
             }
+            zipReader.close();
+            byte[] tempByte = builder.toString().getBytes(StandardCharsets.UTF_8);
+            return reader.Transform(tempByte);
+        } catch(Exception e) {
+            throw new Exception("Error in file unzip. Check selected file, actions and try again.", e);
         }
-        zipReader.close();
-        byte[] tempByte = builder.toString().getBytes(StandardCharsets.UTF_8);
-        return reader.Transform(tempByte);
     }
     
     @Override
-    public ArrayList<ArrayList<String>> Transform(byte[] readFile) throws Throwable {
+    public ArrayList<ArrayList<String>> Transform(byte[] readFile) throws Exception {
         return reader.Transform(readFile);
     }
     
     @Override
-    public ArrayList<ArrayList<String>> Calculate(ArrayList<ArrayList<String>> readFile) throws Throwable {
+    public ArrayList<ArrayList<String>> Calculate(ArrayList<ArrayList<String>> readFile) throws Exception {
         return reader.Calculate(readFile);
     }
     
     @Override
-    public void getResult(String outputFileName) throws Throwable {
+    public void getResult(String outputFileName) throws Exception {
         ArrayList<ArrayList<String>> readFile = Read();
         ArrayList<ArrayList<String>> result = Calculate(readFile);
         WriteResult(result, outputFileName);
