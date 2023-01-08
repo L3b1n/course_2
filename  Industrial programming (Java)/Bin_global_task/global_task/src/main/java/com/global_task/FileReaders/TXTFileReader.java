721eee345a814c7d1f556a3af63e84389748eee3
L3b1n <binleo07@icloud.com>
2023-01-08 15:11:43 +0300

    Add new throws for methods in class TXTFileReader

---  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/FileReaders/TXTFileReader.java
+++  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/FileReaders/TXTFileReader.java
@@ -15,67 +15,90 @@ public class TXTFileReader extends FileReaderInfo {
     }
 
     @Override
-    public void Write(ArrayList<ArrayList<String>> result, String outputFileName) throws Throwable {
-        FileWriter writer = new FileWriter(outputFileName);
-        for(int i = 0; i < result.size(); i++) {
-            writer.write(result.get(i).get(0));
-            if(i != result.size() - 1) {
-                writer.write("\n");
+    public void Write(ArrayList<ArrayList<String>> result, String outputFileName) throws Exception {
+        try {
+            FileWriter writer = new FileWriter(outputFileName);
+            for(int i = 0; i < result.size(); i++) {
+                writer.write(result.get(i).get(0));
+                if(i != result.size() - 1) {
+                    writer.write("\n");
+                }
             }
+            writer.close();
+        } catch(Exception e) {
+            throw new Exception("Error in TXT file write. Check selected file, actions and try again.", e);
         }
-        writer.close();
     }
 
     @Override
-    public void WriteResult(ArrayList<ArrayList<String>> result, String outputFileName) throws Throwable {
-        FileWriter writer = new FileWriter(outputFileName);
-        for(int i = 0; i < result.size(); i++) {
-            writer.write("Result" + Integer.toString(i + 1) + " = " + result.get(i).get(0) + "\n");
+    public void WriteResult(ArrayList<ArrayList<String>> result, String outputFileName) throws Exception {
+        try {
+            FileWriter writer = new FileWriter(outputFileName);
+            for(int i = 0; i < result.size(); i++) {
+                writer.write("Result" + Integer.toString(i + 1) + " = " + result.get(i).get(0) + "\n");
+            }
+            writer.close();
+        } catch(Exception e) {
+            throw new Exception("Error in TXT file calculating. Check selected file, actions and try again.", e);
         }
-        writer.close();
     }
 
     @Override
-    public ArrayList<ArrayList<String>> Read() throws Throwable {
-        ArrayList<ArrayList<String>> readFile = new ArrayList<>();
-        Scanner reader = new Scanner(new FileReader(inputName));
-        while(reader.hasNextLine()) {
-            ArrayList<String> temp = new ArrayList<>();
-            temp.add(reader.nextLine() + "\n");
-            readFile.add(temp);
+    public ArrayList<ArrayList<String>> Read() throws Exception {
+        try {
+            ArrayList<ArrayList<String>> readFile = new ArrayList<>();
+            Scanner reader = new Scanner(new FileReader(inputName));
+            while(reader.hasNextLine()) {
+                ArrayList<String> temp = new ArrayList<>();
+                temp.add(reader.nextLine() + "\n");
+                readFile.add(temp);
+            }
+            reader.close();
+            return readFile;
+        } catch(Exception e) {
+            throw new Exception("Error in TXT file calculating. Check selected file, actions and try again.", e);
         }
-        reader.close();
-        return readFile;
     }
 
     @Override
-    public ArrayList<ArrayList<String>> Transform(byte[] tempByte) throws Throwable {
-        String tempString = new String(tempByte, StandardCharsets.UTF_8);
-        StringTokenizer stringTokenizer = new StringTokenizer(tempString, "\n");
-        ArrayList<ArrayList<String>> readFile = new ArrayList<>();
-        while(stringTokenizer.hasMoreTokens()) {
-            ArrayList<String> temp = new ArrayList<>(); 
-            temp.add(stringTokenizer.nextToken());
-            readFile.add(temp);
+    public ArrayList<ArrayList<String>> Transform(byte[] tempByte) throws Exception {
+        try {
+            String tempString = new String(tempByte, StandardCharsets.UTF_8);
+            StringTokenizer stringTokenizer = new StringTokenizer(tempString, "\n");
+            ArrayList<ArrayList<String>> readFile = new ArrayList<>();
+            while(stringTokenizer.hasMoreTokens()) {
+                ArrayList<String> temp = new ArrayList<>(); 
+                temp.add(stringTokenizer.nextToken());
+                readFile.add(temp);
+            }
+            return readFile;
+        } catch(Exception e) {
+            throw new Exception("Error in TXT file calculating. Check selected file, actions and try again.", e);
         }
-        return readFile;
     }
 
     @Override
-    public ArrayList<ArrayList<String>> Calculate(ArrayList<ArrayList<String>> readFile) throws Throwable {
+    public ArrayList<ArrayList<String>> Calculate(ArrayList<ArrayList<String>> readFile) throws Exception {
         ArrayList<ArrayList<String>> calculated = new ArrayList<>();
         for(ArrayList<String> lines : readFile) {
             for(String line : lines) {
-                ArrayList<String> calculatedLine = new ArrayList<>();
-                calculatedLine.add(Calculation.CalculationOfLine(line));
-                calculated.add(calculatedLine);
+                String tempCalculation = Calculation.CalculationOfLine(line);
+                if(!tempCalculation.equals(line)) {
+                    ArrayList<String> calculatedLine = new ArrayList<>();
+                    calculatedLine.add(tempCalculation);
+                    calculated.add(calculatedLine);
+                }
             }
         }
+        if(calculated.size() == 0) {
+            throw new Exception("Error in TXT file calculating. There aren't any math expressions. Check selected file, actions and try again.");
+        }
         return calculated;
+        
     }
     
     @Override
-    public void getResult(String outputFileName) throws Throwable {
+    public void getResult(String outputFileName) throws Exception {
         ArrayList<ArrayList<String>> readFile = Read();
         ArrayList<ArrayList<String>> result = Calculate(readFile);
         WriteResult(result, outputFileName);
