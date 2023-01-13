03b97e55b2553f98fc08c7c71907eb7b6d4dcc20
L3b1n <binleo07@icloud.com>
2023-01-13 18:54:19 +0300

    Add new methods ReadResult and TransformResult in interface InterfaceFileReader

---  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/contracts/Interface/InterfaceFileReader.java
+++  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/contracts/Interface/InterfaceFileReader.java
@@ -7,7 +7,9 @@ public interface InterfaceFileReader {
     void Write(ArrayList<ArrayList<String>> result, String outputFileName) throws Exception;
     void WriteResult(ArrayList<ArrayList<String>> result, String outputFileName) throws Exception;
     ArrayList<ArrayList<String>> Read() throws Exception;
+    ArrayList<ArrayList<String>> ReadResult() throws Exception;
     ArrayList<ArrayList<String>> Transform(byte[] tempByte) throws Exception;
+    ArrayList<ArrayList<String>> TransformResult(byte[] tempByte) throws Exception;
     ArrayList<ArrayList<String>> Calculate(ArrayList<ArrayList<String>> readFile) throws Exception;
     void getResult(String outputFileName) throws Exception;
 }
