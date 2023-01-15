48b55e0a7679ac5beb3c5bead88c8e369995d4c5
L3b1n <binleo07@icloud.com>
2023-01-15 15:22:09 +0300

    Create new web project for global_task

---
+++  Industrial programming (Java)/Bin_global_task/global_task/rest API/global_task/src/test/java/com/global_task/global_task/GlobalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global_task.global_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
