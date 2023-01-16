7654b36db217ee4f481d0c3c46c60e6ad3f54bb3
L3b1n <binleo07@icloud.com>
2023-01-16 20:04:08 +0300

    Add class TestConstants containing constants for test folder

---
+++  Industrial programming (Java)/Bin_global_task/rest_api/src/test/java/com/rest/rest_api/testsEnum/TestConstants.java
@@ -0,0 +1,23 @@
+package com.rest.rest_api.testsEnum;
+
+public class TestConstants {
+    public static final String DEFAULT_TXT = "./src/test/resources/defaultInput.txt";
+    public static final String DEFAULT_XML = "./src/test/resources/defaultInput.xml";
+    public static final String DEFAULT_JSON = "./src/test/resources/defaultInput.json";
+    public static final String DEFAULT_ZIP = "./src/test/resources/input.zip";
+
+    public static final String ENCRYPTED_TXT = "./src/test/resources/encrypted.txt";
+    public static final String ENCRYPTED_XML = "./src/test/resources/encrypted.xml";
+    public static final String ENCRYPTED_JSON = "./src/test/resources/encrypted.json";
+
+    public static final String COMPRESSED_TXT = "./src/test/resources/compressed.txt";
+    public static final String COMPRESSED_XML = "./src/test/resources/compressed.xml";
+    public static final String COMPRESSED_JSON = "./src/test/resources/compressed.json";
+
+    public static final String OUTPUT_TXT = "./src/main/results/defaultOutput.txt";
+    public static final String OUTPUT_XML = "./src/main/results/defaultOutput.xml";
+    public static final String OUTPUT_JSON = "./src/main/results/defaultOutput.json";
+    public static final String OUTPUT_ZIP = "./src/main/results/defaultOutput.txt";
+
+    public static final String KEY = "squirreldgwndlaj";
+}
