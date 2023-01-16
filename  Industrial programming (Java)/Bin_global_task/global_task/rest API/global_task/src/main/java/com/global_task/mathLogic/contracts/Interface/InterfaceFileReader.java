6844924fd0d8737e50592171d9e7a97e04d55100
L3b1n <binleo07@icloud.com>
2023-01-16 16:17:54 +0300

    Add class InterfaceFileReader to rest API folder

---
+++  Industrial programming (Java)/Bin_global_task/global_task/rest API/global_task/src/main/java/com/global_task/mathLogic/contracts/Interface/InterfaceFileReader.java
@@ -0,0 +1,12 @@
+package com.global_task.mathLogic.contracts.Interface;
+
+import java.util.ArrayList;
+
+public interface InterfaceFileReader {
+    String getFileName();
+    void Write(ArrayList<ArrayList<String>> result, String outputFileName) throws Exception;
+    ArrayList<ArrayList<String>> Read() throws Exception;
+    ArrayList<ArrayList<String>> Transform(byte[] tempByte) throws Exception;
+    ArrayList<ArrayList<String>> Calculate(ArrayList<ArrayList<String>> readFile) throws Exception;
+    void getResult(String outputFileName) throws Exception;
+}
