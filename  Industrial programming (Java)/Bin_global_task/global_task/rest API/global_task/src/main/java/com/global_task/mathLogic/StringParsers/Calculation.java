123591c0832e767df56ec3c127ba155e6b689b1a
L3b1n <binleo07@icloud.com>
2023-01-16 16:20:55 +0300

    Add class Calculation to rest API folder

---
+++  Industrial programming (Java)/Bin_global_task/global_task/rest API/global_task/src/main/java/com/global_task/mathLogic/StringParsers/Calculation.java
@@ -0,0 +1,39 @@
+package com.global_task.mathLogic.StringParsers;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import org.mariuszgromada.math.mxparser.Expression;
+
+public class Calculation {
+    public static String CalculationOfLine(String temp_line) {
+        Expression newExpression = new Expression(temp_line);
+        if(newExpression.checkSyntax()) {
+            double result = newExpression.calculate();
+            if(Double.isNaN(result)) {
+                return devisionError;
+            }
+            return String.valueOf(result);
+        }
+        return temp_line;
+    }
+
+    public static void Calculate(String inputName, String outputName) throws IOException {
+        File inputFile = new File(inputName);
+        FileFilter outputFile = (FileFilter) new FileWriter(outputName);
+        Scanner reader = new Scanner(inputFile);
+        while(reader.hasNextLine()) {
+            String line = reader.nextLine();
+            String calculatedLine = CalculationOfLine(line);
+            ((OutputStreamWriter) outputFile).write(calculatedLine);
+        }
+        ((OutputStreamWriter) outputFile).close();
+        reader.close();
+    }
+
+    private static final String devisionError = "Error: devision by zero";
+}
