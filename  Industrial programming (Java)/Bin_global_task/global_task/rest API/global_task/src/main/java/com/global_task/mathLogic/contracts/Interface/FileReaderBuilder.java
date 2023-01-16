99648af562611e5f659755614f6b58d7ec087353
L3b1n <binleo07@icloud.com>
2023-01-16 16:17:19 +0300

    Add class FileReaderBuilder to rest API folder

---
+++  Industrial programming (Java)/Bin_global_task/global_task/rest API/global_task/src/main/java/com/global_task/mathLogic/contracts/Interface/FileReaderBuilder.java
@@ -0,0 +1,11 @@
+package com.global_task.mathLogic.contracts.Interface;
+
+import java.util.ArrayList;
+
+public interface FileReaderBuilder {
+    void setEncrypt(String key);
+    void setEncrypt(ArrayList<String> keyList);
+    void setCompressed(boolean isCompressed);
+    void setZip(boolean isZip);
+    InterfaceFileReader getFileReader();
+}
