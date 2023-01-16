3bb4e13f4a1c74c23e0e545e24661bf40290aa96
L3b1n <binleo07@icloud.com>
2023-01-16 20:03:39 +0300

    Add RestApiApplicationTests test

---
+++  Industrial programming (Java)/Bin_global_task/rest_api/src/test/java/com/rest/rest_api/RestApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.rest.rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
