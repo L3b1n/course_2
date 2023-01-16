7cb5d97f4e9443308c99f672560c1ee2ae28203f
L3b1n <binleo07@icloud.com>
2023-01-16 16:20:40 +0300

    Add class ZippedFileReader to rest API folder

---
+++  Industrial programming (Java)/Bin_global_task/global_task/rest API/global_task/src/main/java/com/global_task/mathLogic/FileReaders/ZippedFileReader.java
@@ -0,0 +1,77 @@
+package com.global_task.mathLogic.FileReaders;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+import com.global_task.contracts.Interface.InterfaceFileReader;
+
+public class ZippedFileReader extends FileReader {
+    public ZippedFileReader(InterfaceFileReader fileReader) {
+        super(fileReader);
+    }
+
+    public static void WriteZipped(String inputFileName, String outputFileName) throws Exception {
+        try {
+            ZipOutputStream writer = new ZipOutputStream(new FileOutputStream(outputFileName));
+            FileInputStream reader = new FileInputStream(inputFileName);
+            ZipEntry entry = new ZipEntry(inputFileName);
+            writer.putNextEntry(entry);
+            byte[] buffer = new byte[reader.available()];
+            reader.read(buffer);
+            writer.write(buffer);
+            writer.closeEntry();
+            writer.close();
+            reader.close();
+        } catch(Exception e) {
+            throw new Exception("Error in file zip. Check selected file, actions and try again.", e);
+        }
+    }
+
+    @Override
+    public void Write(ArrayList<ArrayList<String>> result, String outputFileName) throws Exception {
+        reader.Write(result, outputFileName);
+    }
+
+    @Override
+    public ArrayList<ArrayList<String>> Read() throws Exception {
+        try {
+            byte[] buffer = new byte[1024];
+            ZipInputStream zipReader = new ZipInputStream(new FileInputStream(inputName));
+            StringBuilder builder = new StringBuilder();
+            while(zipReader.getNextEntry() != null) {
+                int length;
+                while((length = zipReader.read(buffer)) > 0) {
+                    String tempString = new String(buffer, 0, length, StandardCharsets.UTF_8);
+                    builder.append(tempString);
+                }
+            }
+            zipReader.close();
+            byte[] tempByte = builder.toString().getBytes(StandardCharsets.UTF_8);
+            return reader.Transform(tempByte);
+        } catch(Exception e) {
+            throw new Exception("Error in file unzip. Check selected file, actions and try again.", e);
+        }
+    }
+    
+    @Override
+    public ArrayList<ArrayList<String>> Transform(byte[] readFile) throws Exception {
+        return reader.Transform(readFile);
+    }
+    
+    @Override
+    public ArrayList<ArrayList<String>> Calculate(ArrayList<ArrayList<String>> readFile) throws Exception {
+        return reader.Calculate(readFile);
+    }
+    
+    @Override
+    public void getResult(String outputFileName) throws Exception {
+        ArrayList<ArrayList<String>> readFile = Read();
+        ArrayList<ArrayList<String>> result = Calculate(readFile);
+        Write(result, outputFileName);
+    }
+}
