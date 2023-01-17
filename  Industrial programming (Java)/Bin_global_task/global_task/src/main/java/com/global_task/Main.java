9ed0b8a7806ad1f8c9aafd296fbf81fb778d69a2
L3b1n <binleo07@icloud.com>
2023-01-17 16:55:37 +0300

    Add some fixes in main server

---  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/Main.java
+++  Industrial programming (Java)/Bin_global_task/global_task/src/main/java/com/global_task/Main.java
@@ -8,19 +8,19 @@ public class Main {
         while(true) {
             Server serverGet = new Server();
             try {
-                serverGet.createServer(9527);
+                serverGet.createServer(4444);
                 System.out.println("Please wait");
                 serverGet.getFile();
                 serverGet.createNewFile();
                 Server serverSend = new Server();
-                serverSend.createServer(9528);
+                serverSend.createServer(4445);
                 serverSend.sendResult("");
                 serverSend.stopServer();
                 System.out.println("Done");
                 serverGet.stopServer();
             } catch(Throwable e) {
                 Server serverSend = new Server();
-                serverSend.createServer(9528);
+                serverSend.createServer(4445);
                 Server.deleteFile(serverGet.getFileName());
                 serverSend.sendResult(e.toString().substring(e.toString().indexOf(':') + 1));
                 serverSend.stopServer();
