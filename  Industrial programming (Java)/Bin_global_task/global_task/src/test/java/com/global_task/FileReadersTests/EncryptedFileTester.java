a86a5323b313bfd29189e585130f47fbbb92f222
L3b1n <binleo07@icloud.com>
2023-01-17 15:31:29 +0300

    Add new test class EncryptedFileTester to folder test

---
+++  Industrial programming (Java)/Bin_global_task/global_task/src/test/java/com/global_task/FileReadersTests/EncryptedFileTester.java
@@ -0,0 +1,62 @@
+package com.global_task.FileReadersTests;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.global_task.FileReaders.FileBuilder;
+import com.global_task.contracts.Interface.InterfaceFileReader;
+
+public class EncryptedFileTester {
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(output));
+    }
+    
+    @Test
+    public void TestGetResult() {
+        try {
+            FileBuilder builder = new FileBuilder("txt", "./src/main/tests/com/global_task/encrypted.txt");
+            builder.setEncrypt("squirreldgwndlaj");
+            InterfaceFileReader reader = builder.getFileReader();
+            reader.getResult("./src/main/results/com/global_task/test.txt");
+            builder = new FileBuilder("txt", "./src/main/results/com/global_task/test.txt");
+            reader  = builder.getFileReader();
+            Assert.assertEquals("[[Result1 = 545.468], [Result2 = Error: devision by zero], [Result3 = 3.0]]", reader.Read().toString());
+        } catch(Exception e) {
+            Assert.assertEquals("Error in TXT file calculating. Check selected file, actions and try again.", e.toString().substring(e.toString().indexOf(':') + 2));
+        }
+    }
+
+    @Test
+    public void TestGetResult2() {
+        try {
+            FileBuilder builder = new FileBuilder("txt", "./src/main/tests/com/global_task/doubleEncrypted.txt");
+            builder.setEncrypt("squirreldgwndlaj");
+            builder.setEncrypt("abshgsfrenhlksmp");
+            InterfaceFileReader reader = builder.getFileReader();
+            reader.getResult("./src/main/results/com/global_task/test.txt");
+            builder = new FileBuilder("txt", "./src/main/results/com/global_task/test.txt");
+            reader  = builder.getFileReader();
+            Assert.assertEquals("[[Result1 = 545.468], [Result2 = Error: devision by zero], [Result3 = 3.0]]", reader.Read().toString());
+        } catch(Exception e) {
+            Assert.assertEquals("Error in TXT file calculating. Check selected file, actions and try again.", e.toString().substring(e.toString().indexOf(':') + 2));
+        }
+    }
+    
+    @Test
+    public void TestGetResultException() {
+        try {
+            FileBuilder builder = new FileBuilder("txt", "./src/main/results/com/global_task/encrypted.txt");
+            InterfaceFileReader reader = builder.getFileReader();
+            Assert.assertEquals("[[Result1 = 545.468], [Result2 = Error: devision by zero], [Result3 = 3.0]]", reader.Read().toString());
+        } catch(Exception e) {
+            Assert.assertEquals("Error in TXT file read. Check selected file, actions and try again.", e.toString().substring(e.toString().indexOf(':') + 2));
+        }
+    }
+}
