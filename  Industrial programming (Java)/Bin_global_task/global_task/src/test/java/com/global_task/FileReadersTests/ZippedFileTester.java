9e68fce4c26a5b52b2aa452c22e6124eb6154066
L3b1n <binleo07@icloud.com>
2023-01-17 15:27:08 +0300

    Add new test class ZippedFileTester to folder test

---
+++  Industrial programming (Java)/Bin_global_task/global_task/src/test/java/com/global_task/FileReadersTests/ZippedFileTester.java
@@ -0,0 +1,46 @@
+package com.global_task.FileReadersTests;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.global_task.FileReaders.FileBuilder;
+import com.global_task.contracts.Interface.InterfaceFileReader;
+
+public class ZippedFileTester {
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(output));
+    }
+    
+    @Test
+    public void TestGetResult() {
+        try {
+            FileBuilder builder = new FileBuilder("zip", "./src/main/tests/com/global_task/test.zip");
+            builder.setZip(true);
+            InterfaceFileReader reader = builder.getFileReader();
+            reader.getResult("./src/main/results/com/global_task/test.txt");
+            builder = new FileBuilder("txt", "./src/main/results/com/global_task/test.txt");
+            Assert.assertEquals("[[Result1 = 545.468], [Result2 = Error: devision by zero], [Result3 = 3.0]]", reader.Read().toString());
+        } catch(Exception e) {
+            Assert.assertEquals("Error in TXT file calculating. Check selected file, actions and try again.", e.toString().substring(e.toString().indexOf(':') + 2));
+        }
+    }
+    
+    @Test
+    public void TestGetResultException() {
+        try {
+            FileBuilder builder = new FileBuilder("txt", "./src/main/results/com/global_task/test.zip");
+            builder.setZip(true);
+            InterfaceFileReader reader = builder.getFileReader();
+            Assert.assertEquals("[[Result1 = 545.468], [Result2 = Error: devision by zero], [Result3 = 3.0]]", reader.Read().toString());
+        } catch(Exception e) {
+            Assert.assertEquals("Error in file unzip. Check selected file, actions and try again.", e.toString().substring(e.toString().indexOf(':') + 2));
+        }
+    }
+}
