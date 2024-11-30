568d4f7000f803b82326322dde302e47d8821071
L3b1n <binleo07@icloud.com>
2024-11-30 21:18:30 +0300

    Add task 18 (files) of dynamic programming

---
+++ Algorithms (C++)/Dynamic programming/task 18 (files)/files.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <fstream>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(0);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    int k, n, l, i = 0;
+    std::cin >> k >> n;
+    long long sum = 0ll;
+    std::vector<int> answer(n);
+    answer[0] = k; answer[1] = std::pow(k, 2);
+    std::cout << std::accumulate(answer.begin(), answer.end(), sum,
+        [&i, &k, &answer](long long& sum, int& temp) -> long long {
+            if(i >= 2){ temp = (answer[i - 2] + answer[i - 1]) * k;} i++;
+            return sum += temp;
+        }
+    );
+    return 0;
+}
